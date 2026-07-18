bfcb0d2cde29a86e89195632f16350a2dec577df
Veldkornet <Veldkornet@users.noreply.github.com>
2026-07-18 12:45:38 +0200

    chore: remove ad-hoc js test scripts

--- test_hmac_special.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto-js');
-const secretKey = "special_ç_key";
-const signString = "test_sign_string_with_é_special";
-const hmac = crypto.HmacSHA512(signString, secretKey);
-console.log(crypto.enc.Base64.stringify(hmac));
